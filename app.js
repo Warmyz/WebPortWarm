75eb501f19489daa2488c0662935ea478e4cee89
Warmyz <93852296+Warmyz@users.noreply.github.com>
2023-09-17 17:40:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function typeWriter(element, text, index, speed) {
 }
 
 // Typing effect on rackroll_ text
-const rickrolling = "Warakorn Kornpongkun";
+const rickrolling = "W";
 typeWriter(warm, rickrolling, 0, 50);
 
 const sections = document.querySelectorAll('section');
